9756e23be5f04ef70568e304471334c5825e5f26
garibaycarlos <garibaycarlos@live.com>
2021-03-15 14:06:21 -0600

    Decorated enum adding spaces.

--- Spice/Models/MenuItem.cs
+++ Spice/Models/MenuItem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace Spice.Models
@@ -18,8 +19,10 @@ namespace Spice.Models
         public enum ESpicy
         {
             NA = 0,
+            [Display(Name = "Not Spicy")]
             NotSpicy = 1,
             Spicy = 2,
+            [Display(Name = "Very Spicy")]
             VerySpicy = 3
         }
 
