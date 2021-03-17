e8c72577a07738dc674b2cfae58e9801e8de92e6
garibaycarlos <garibaycarlos@live.com>
2021-03-17 14:24:21 -0600

    Added page roles and shopping cart count.

--- Spice/Utility/SD.cs
+++ Spice/Utility/SD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,10 @@ namespace Spice.Utility
     public static class SD
     {
         public const string DefaultFoodImage = "default_food.png";
+        public const string ManagerUser = "Manager";
+        public const string KitchenUser = "Kitchen";
+        public const string FrontDeskUser = "FrontDesk";
+        public const string CustomerEndUser = "Customer";
+        public const string ssShoppingCartCount = "ssCartCount";
     }
 }
